a4282c4ed1b5612145ebd25d27903bfd2a5495b5
Mario Scheliga <marsch.js@gmail.com>
2018-05-04 17:13:24 +0200

    added babel, eslint, moved tarots to assets, added asset copy task

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  type: 'react-app'
+require('dotenv').config()
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const isDev = process.env.NODE_ENV !== 'production'
+module.exports = function(nwb) {
+  return {
+    type: 'react-app',
+    babel: {
+      presets: ['es2015', 'stage-0', 'react']
+    },
+    webpack: {
+      extra: {
+        resolve: {
+          modules: ['node_modules', './src']
+        },
+        plugins: [
+          new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets' },
+          ])
+        ]
+      }
+    }
+  }
 }
